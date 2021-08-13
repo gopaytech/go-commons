8e7e8b136f28c762f13ec7ac0d1b676ce28ccbbd
Deny Prasetyo <jasoet87@gmail.com>
2021-08-13 10:05:53 +0700

    fix: remove wrong implementation of NewNameWithClient

--- pkg/gitlab/gitlab.go
+++ pkg/gitlab/gitlab.go
@@ -19,15 +19,6 @@ func NewName(name string) NameOrId {
 	return NameOrId{Name: name}
 }
 
-func NewNameWithClient(name string, client *gl.Client) NameOrId {
-	baseUrl := client.BaseURL().String()
-	name = strings.TrimPrefix(name, baseUrl)
-	name = strings.TrimPrefix(name, "/")
-	name = strings.TrimSuffix(name, "/")
-
-	return NameOrId{Name: name}
-}
-
 func NewNameWithBaseUrl(name string, baseUrl string) NameOrId {
 	name = strings.TrimPrefix(name, baseUrl)
 	name = strings.TrimPrefix(name, "/")
