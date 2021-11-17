f3f1b5924f499e5aabd29128d9db54cf72ecdc4e
William Albertus Dembo <29192168+walbertus@users.noreply.github.com>
2021-11-17 14:33:59 +0700

    feat: add gitlab search projects
    Co-authored-by: Eko Simanjuntak <ecojuntak@gmail.com>

---
+++ pkg/gitlab/search.go
@@ -0,0 +1,20 @@
+package gitlab
+
+import gl "github.com/xanzy/go-gitlab"
+
+type Search interface {
+	ProjectsByGroup(parent NameOrId, name string) ([]*gl.Project, *gl.Response, error)
+}
+
+type search struct {
+	client *gl.Client
+}
+
+func (s *search) ProjectsByGroup(parent NameOrId, name string) ([]*gl.Project, *gl.Response, error) {
+	opts := &gl.SearchOptions{}
+	return s.client.Search.ProjectsByGroup(parent.ID, name, opts)
+}
+
+func NewSearch(client *gl.Client) Search {
+	return &search{client: client}
+}
