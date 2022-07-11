49c66ec6567a7bd7cfe9949e1274a7082a2fd0a3
Eko Simanjuntak <ecojuntak@gmail.com>
2022-07-11 14:55:48 +0800

    feat: add functionality to create protected branch

---
+++ pkg/gitlab/protected_branch.go
@@ -0,0 +1,22 @@
+package gitlab
+
+import gl "github.com/xanzy/go-gitlab"
+
+type ProtectedBranch interface {
+    CreateProtectedBranch(projectID NameOrId, branchName string) (*gl.ProtectedBranch, *gl.Response, error)
+}
+
+type protectedBranch struct {
+    client *gl.Client
+}
+
+func (p *protectedBranch) CreateProtectedBranch(projectID NameOrId, branchName string) (*gl.ProtectedBranch, *gl.Response, error) {
+    opts := &gl.ProtectRepositoryBranchesOptions{
+        Name: &branchName,
+    }
+    return p.client.ProtectedBranches.ProtectRepositoryBranches(projectID, opts)
+}
+
+func NewProtectedBranch(client *gl.Client) ProtectedBranch {
+    return &protectedBranch{client: client}
+}
