52336c85e077c30b8498b23b0fdef91580d94fd6
Jasoet Martohartono <jasoet87@gmail.com>
2023-02-27 13:16:41 +0700

    feat: add Gitlab Group Env with Parent

--- pkg/gitlab/group_variables.go
+++ pkg/gitlab/group_variables.go
@@ -18,6 +18,39 @@ func (gv *groupVariables) GetVariable(gid interface{}, key string) (*gl.GroupVar
 
 	return result, nil
 }
+func (gv *groupVariables) GetVariables(groupID int, recursive bool) (map[string]string, error) {
+	variables := make(map[string]string)
+	group, _, err := gv.client.Groups.GetGroup(groupID, &gl.GetGroupOptions{})
+	if err != nil {
+		return variables, err
+	}
+
+	for group != nil {
+		v, _, err := gv.client.GroupVariables.ListVariables(groupID, &gl.ListGroupVariablesOptions{})
+		if err != nil {
+			return variables, err
+		}
+		for _, variable := range v {
+			variables[variable.Key] = variable.Value
+		}
+
+		if recursive == false {
+			break
+		}
+
+		groupID = group.ParentID
+		if groupID != 0 {
+			group, _, err = gv.client.Groups.GetGroup(groupID, &gl.GetGroupOptions{})
+			if err != nil {
+				return variables, err
+			}
+		} else {
+			group = nil
+		}
+	}
+
+	return variables, nil
+}
 
 func NewGroupVariable(client *gl.Client) GroupVariables {
 	return &groupVariables{client: client}
